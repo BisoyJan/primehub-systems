d4cc3986c3ed33b3b00f632c8e4546de058c2bb3
BisoyJan <bisoyjan@gmail.com>
2026-02-16 13:57:12 +0800

    feat(avatar-crop-dialog): add objectFit property to crop component for better image fitting

--- resources/js/components/avatar-crop-dialog.tsx
+++ resources/js/components/avatar-crop-dialog.tsx
@@ -124,6 +124,7 @@ export function AvatarCropDialog({ open, imageSrc, onCropComplete, onClose }: Av
                         aspect={1}
                         cropShape="round"
                         showGrid={false}
+                        objectFit="contain"
                         onCropChange={onCropChange}
                         onZoomChange={onZoomChange}
                         onCropComplete={handleCropComplete}
